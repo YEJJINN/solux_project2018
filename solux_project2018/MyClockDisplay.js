fc4e28071630b3d8def03dfad6b8dd0d7fb8266d
YEJJINN <45589632+YEJJINN@users.noreply.github.com>
2018-12-05 11:14:03 +0900

    Add files via upload

---
+++ solux_project2018/MyClockDisplay.js
@@ -0,0 +1,16 @@
+function clock() {// We create a new Date object and assign it to a variable called "time".
+var time = new Date(),
+    hours = time.getHours(),
+    minutes = time.getMinutes(),
+    seconds = time.getSeconds();
+    
+document.querySelectorAll('.clock')[0].innerHTML = harold(hours) + ":" + harold(minutes) + ":" + harold(seconds);
+
+  function harold(standIn) {
+    if (standIn < 10) {
+      standIn = '0' + standIn
+    }
+    return standIn;
+  }
+}
+setInterval(clock, 1000);
